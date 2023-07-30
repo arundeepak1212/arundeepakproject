7659e92953bbfb17f45e9ad4dbf569ff05c95dbd
arundeepak1212 <arundeepak12121997@gmail.com>
2023-07-30 22:58:43 +0530

    first push1

--- src/org/demo/Test.java
+++ src/org/demo/Test.java
@@ -3,6 +3,7 @@ package org.demo;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("done");
-	System.out.println("pachisiva");
+	System.out.println("arun");
+	System.out.println("smart arun");
 }
 }
